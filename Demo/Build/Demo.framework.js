9e678f3c8b4fe518474f896409cea87ffbb58615
André Cerqueira <78262628+AndreCerqueira@users.noreply.github.com>
2022-04-03 15:16:40 +0100

    new demo

--- Demo/Build/Demo.framework.js
+++ Demo/Build/Demo.framework.js
@@ -1305,7 +1305,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 8747792;
+STATICTOP = STATIC_BASE + 8747856;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3371,7 +3371,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 8747792;
+var STATIC_BUMP = 8747856;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
