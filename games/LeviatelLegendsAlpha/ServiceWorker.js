609cad7185dc7dbc5466d3ffdaedc48ee31c54bb
André Cerqueira <78262628+AndreCerqueira@users.noreply.github.com>
2025-05-12 20:32:39 +0100

    .

--- games/LeviatelLegendsAlpha/ServiceWorker.js
+++ games/LeviatelLegendsAlpha/ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "NestGames-Leviatel Legends-1.0.0";
 const contentToCache = [
     "Build/2e5176df4d4190a073212479c79dbdb4.loader.js",
     "Build/5e3737f539080891623c4dc3cb9acaef.framework.js",
-    "Build/6f5ab238e8a151347f5da15c2482d875.data",
+    "Build/6a28816879cb4303aeec813fe529cafe.data",
     "Build/52218d20f0e99471b122a17a4f8b4086.wasm",
     "TemplateData/style.css"
 
